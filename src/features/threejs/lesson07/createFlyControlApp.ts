74249cadb792c05ece6e6dfec92c4f8c5aa351aa
whiteand <andrewbeletskiy@gmail.com>
2023-09-25 13:49:44 +0300

    feat: added orbit control

---
+++ src/features/threejs/lesson07/createFlyControlApp.ts
@@ -0,0 +1,91 @@
+import { BehaviorSubject, fromEvent, map } from 'rxjs'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { createApp } from '~/packages/interactive-app'
+
+export default function createFlyControlApp(
+  canvas: HTMLCanvasElement,
+  size$: BehaviorSubject<THREE.Vector2>,
+) {
+  return createApp(
+    () => {
+      // Building Scene
+      const scene = new THREE.Scene()
+
+      const clock = new THREE.Clock()
+
+      const axesHelper = new THREE.AxesHelper()
+      scene.add(axesHelper)
+
+      const cube = new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1, 1),
+        new THREE.MeshBasicMaterial({
+          color: 0x336699,
+        }),
+      )
+      scene.add(cube)
+
+      // Camera
+      const camera = new THREE.PerspectiveCamera(
+        55,
+        size$.getValue().x / size$.getValue().y,
+      )
+
+      camera.position.set(2, 2, 2)
+      camera.lookAt(cube.position)
+
+      scene.add(camera)
+
+      // Renderer
+      const renderer = new THREE.WebGLRenderer({
+        canvas,
+        alpha: false,
+      })
+
+      renderer.setSize(size$.getValue().x, size$.getValue().y)
+
+      const controls = new OrbitControls(camera, renderer.domElement)
+
+      renderer.render(scene, camera)
+
+      const subscription = size$.subscribe((sizes) => {
+        renderer.setSize(sizes.x, sizes.y)
+        camera.aspect = sizes.x / sizes.y
+        camera.updateProjectionMatrix()
+      })
+
+      const mousePosition$ = new BehaviorSubject<THREE.Vector2 | null>(null)
+
+      subscription.add(
+        fromEvent<MouseEvent>(canvas, 'mousemove')
+          .pipe(map((event) => new THREE.Vector2(event.offsetX, event.offsetY)))
+          .subscribe(mousePosition$),
+      )
+
+      subscription.add(
+        fromEvent(canvas, 'mouseout')
+          .pipe(map(() => null))
+          .subscribe(mousePosition$),
+      )
+
+      return {
+        renderer,
+        scene,
+        camera,
+        controls,
+        cube,
+        subscription,
+        mousePosition$,
+        clock,
+      }
+    },
+    ({ scene, camera, renderer }) => {
+      renderer.render(scene, camera)
+    },
+    ({ subscription, clock, controls }) => {
+      subscription.unsubscribe()
+      clock.stop()
+      controls.dispose()
+    },
+  )
+}
--- src/features/threejs/lesson07/index.tsx
+++ src/features/threejs/lesson07/index.tsx
@@ -1,5 +1,6 @@
 import { FeatureConstructor } from '~/architecture/types'
 import CanvasApp from '~/components/CanvasApp'
+import createFlyControlApp from './createFlyControlApp'
 import createMouseControlApp from './createMouseControlApp'
 
 export const Lesson07Feature: FeatureConstructor = (app) => {
@@ -9,4 +10,10 @@ export const Lesson07Feature: FeatureConstructor = (app) => {
     .setTitle('THREE.js | Mouse Control')
     .setElement(<CanvasApp createApp={createMouseControlApp} />)
     .showInMenu('main', 'Mouse Control', 0)
+  app
+    .child()
+    .setPath('/fly-control')
+    .setTitle('THREE.js | Fly Control')
+    .setElement(<CanvasApp createApp={createFlyControlApp} />)
+    .showInMenu('main', 'Fly Control', 0)
 }
